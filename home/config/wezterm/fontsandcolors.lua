c1dc2d9a2e4a497b95af3fee3e2aaea639397a8f
Matthew <matthew@blu.shortofcode.net>
2024-04-11 02:12:58 -0400

    try to deal with font rendering wierdness

--- home/config/wezterm/fontsandcolors.lua
+++ home/config/wezterm/fontsandcolors.lua
@@ -5,18 +5,9 @@ local module = {}
 
 function module.apply(config)
     config.color_scheme = 'Railscasts (dark) (terminal.sexy)'
-    config.window_frame = {
-       font_size = 8.0,
-       font = wezterm.font { family = 'Fira Code', weight = 'Light' }
-    }
-
-    config.font = wezterm.font {
-        family = 'Noto Sans Mono',
-        weight = 'Regular'
-    }
-
     config.font_size = 9.0
     config.line_height = 1.1
+    config.front_end = 'WebGpu'
 end
 
 return module
