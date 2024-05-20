93599a947fa35fc24a2c02c55dd048178c322779
Matthew <matthew@blu.shortofcode.net>
2024-05-20 17:31:48 -0400

    lets try a transparent wezterm bkgd

--- home/config/wezterm/fontsandcolors.lua
+++ home/config/wezterm/fontsandcolors.lua
@@ -8,6 +8,7 @@ function module.apply(config)
     config.font_size = 9.0
     config.line_height = 1.1
     config.front_end = 'WebGpu'
+    config.window_background_opacity = 0.9
 end
 
 return module
