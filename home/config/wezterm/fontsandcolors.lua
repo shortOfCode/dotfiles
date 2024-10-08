80f9d12530dee7fa739081a8b262425de7c33cf5
Matthew <matthew@blu.shortofcode.net>
2024-10-08 17:31:20 -0400

    make wezterm config adaptable for my macbook

--- home/config/wezterm/fontsandcolors.lua
+++ home/config/wezterm/fontsandcolors.lua
@@ -7,8 +7,20 @@ function module.apply(config)
     config.color_scheme = 'Railscasts (dark) (terminal.sexy)'
     config.font_size = 9.0
     config.line_height = 1.1
-    config.front_end = 'OpenGL'
     config.window_background_opacity = 0.8
+    if Uname == 'Darwin' then
+        print('Using Darwin Graphics')
+        config.webgpu_preferred_adapter = {
+            backend = "Metal",
+            device = 0,
+            device_type = "IntegratedGpu",
+            name = "Apple M1 Pro",
+            vendor = 0
+        }
+        config.front_end = 'WebGpu'
+    else
+        config.front_end = 'OpenGL'
+    end
 end
 
 return module
--- home/config/wezterm/wezterm.lua
+++ home/config/wezterm/wezterm.lua
@@ -3,6 +3,11 @@ local fontsandcolors = require 'fontsandcolors'
 local keyboard = require 'keyboard'
 
 local config = {}
+local handle = io.popen('uname')
+if handle ~= nil then
+    Uname = handle:read("*a")
+    handle:close()
+end
 
 fontsandcolors.apply(config)
 keyboard.apply(config)
